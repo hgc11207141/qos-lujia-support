6a186364f1f48eef9032456f15bd66f64a745797
zhouya1234554321 <632394831@qq.com>
2020-05-13 11:05:55 +0800

    3院实时表修改

--- src/main/java/com/paladin/qos/analysis/impl/yiyuan/opd/OutpatientNum.java
+++ src/main/java/com/paladin/qos/analysis/impl/yiyuan/opd/OutpatientNum.java
@@ -30,7 +30,7 @@ public class OutpatientNum extends YiyuanDataProcessor{
 	@Override
 	public long getTotalNum(Date startTime, Date endTime, String unitId) {
 		sqlSessionContainer.setCurrentDataSource(getDataSourceByUnit(unitId));
-		if(unitId.equals("320583467170249")||unitId.equals("320583467170257")||unitId.equals("320583467170513")||unitId.equals("320583467170265")){
+		if(unitId.equals("320583467170249")||unitId.equals("320583467170257")||unitId.equals("320583467170513")){
 		    if(startTime.getTime()<= StatisticsConstant.TABLE_MEDICALRECORD_BF_START_TIME){
 			return sqlSessionContainer.getSqlSessionTemplate().getMapper(OpdStatisticsMapper.class).outpatientNum(startTime, endTime);
 		    }else{
